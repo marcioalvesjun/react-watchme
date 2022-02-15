3d0a5baa870df94da5f33576e70d7eebc681df0f
Marcio Alves <marcioalvesjun@Marcios-MacBook-Air.local>
2022-02-15 00:05:50 -0300

    created components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import './header.scss'
+
+interface HeaderProps {
+  selectedGenre: GenreResponseProps;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
+export function Header({ selectedGenre }: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span>{selectedGenre.title}</span></span>
+    </header>
+  )
+}
